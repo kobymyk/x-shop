a72925b89b0eaa8ba0cdffd54bcd0a2b4513580f
kobymyk <kobylkin@gmail.com>
2018-11-19 22:19:57 +0200

    end

--- src/main/java/db2/onlineshop/web/templater/PageGenerator.java
+++ src/main/java/db2/onlineshop/web/templater/PageGenerator.java
@@ -4,7 +4,6 @@ import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
@@ -26,7 +25,8 @@ public class PageGenerator {
     public String getPage(String filename, Map<String, Object> data) {
         Writer stream = new StringWriter();
         try {
-            Template template = cfg.getTemplate(HTML_DIR + File.separator + filename + ".html");
+            // HTML_DIR + File.separator +
+            Template template = cfg.getTemplate(filename + ".html");
             template.process(data, stream);
         } catch (IOException | TemplateException e) {
             throw new RuntimeException(e);
@@ -36,5 +36,10 @@ public class PageGenerator {
 
     private PageGenerator() {
         cfg = new Configuration();
+        cfg.setClassForTemplateLoading(this.getClass(), "/" + HTML_DIR);
+    }
+
+    public Configuration getCfg() {
+        return cfg;
     }
 }
